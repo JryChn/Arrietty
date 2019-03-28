9bea94e92d5eecdbb69ad4309decb5baf3400d0a
Jeremy Chen <jeremychen9412@gmail.com>
2019-03-28 16:56:34 +0800

    add install.sh

---
+++ install/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ====================================================
+#   Copyright (C)2019 All rights reserved.
+#
+#   Author        : Jeremy Chen
+#   Email         : jeremychen@djeremychen.com
+#   File Name     : install.sh
+#   Last Modified : 2019-03-28 16:53
+#   Describe      :
+#
+# ====================================================
+
+
+
+#need install: i3-wm,urxvt,zsh,vim-python,pgit,curl,wget,cmake,gcc,
+#ctag-exuberant,fonts,qutebrowser,virt-manager,qemu,python3,python_dev,python3dev
