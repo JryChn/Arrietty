8d7c1c7dc78bf67da2b85fd181015f75743bf70b
Jeremy Chen <jeremychen9412@gmail.com>
2019-04-04 16:39:27 +0800

    update the install.sh

--- install/install.sh
+++ install/install.sh
@@ -13,5 +13,7 @@
 
 
 #need install: urxvt,zsh,vim-python,git,curl,wget,cmake,gcc,
-#ctag-exuberant,fonts,qutebrowser,virt-manager,qemu,python3,python_dev,python3dev
+#ctag-exuberant,fonts,proxychains,shadowsocks-libev,qutebrowser,virt-manager,qemu,python3,python_dev,python3dev,vlc,feh,wqy-microhei,fcitx-rime
 #i3-gaps,i3lock-fancy,i3status,polybar,compton,rofi,fontawesome
+#nvidia-drivers,xf86-video-intel,xorg-server, lightdm 
+#sysklogd,cronie,layman,alsa-lib, alsa-utils,pulseaudio,
