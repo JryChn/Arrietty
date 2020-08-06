ffd5d4988a88eeef5d1090f863c23bb4455c7cfe
JryChn <jeremychen@djeremychen.com>
2020-08-07 02:38:54 +0800

    update background script

--- dotfile/.config/sway/scripts/autochangeWallpaper.sh
+++ dotfile/.config/sway/scripts/autochangeWallpaper.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
+#change a new Wallpaper
+WALLPAPER=`ls $HOME/.config/sway/wallpapers | sort -R | head -n 1`
+echo $WALLPAPER
+swaymsg "output * bg $HOME/.config/sway/wallpapers/'$WALLPAPER'  stretch"
+
 #Get some pics in some condition
 LINECOUNTS=`ls $HOME/.config/sway/wallpapers | wc -l`
 if [ $LINECOUNTS -le 10 ]
 then
   for loop in 1 2 3 4 5
   do
-    wget -qb  -O "$HOME/.config/sway/wallpapers/wallpaper$RANDOM.jpg" "https://source.unsplash.com/1920x1080/?wallpapers,nature"
+    wget -qb -P $HOME/.config/sway/wallpapers "https://source.unsplash.com/1920x1080/?wallpapers,nature"
   done
 fi
 
 
 
-WALLPAPER=`ls $HOME/.config/sway/wallpapers | sort -R | head -n 1`
-swaymsg "output * bg $WALLPAPER  stretch"
 
 #Delete some politics
 if [ $LINECOUNTS -ge 10 ]
 then
+  echo `ls $HOME/.config/sway/wallpapers | sort -R | head -n 1`
   for loop in 1 2 3 4 5
   do
-    rm `ls $HOME/.config/sway/wallpapers | sort -R | head -n 1`
+    rm $HOME/.config/sway/wallpapers/`ls $HOME/.config/sway/wallpapers | sort -R | head -n 1`
   done
 fi
