0e1d272a628855a5a301518417eb144d29a66749
JryChn <jeremychen@djeremychen.com>
2020-08-07 02:45:45 +0800

    update background script

--- dotfile/.config/sway/scripts/autochangeWallpaper.sh
+++ dotfile/.config/sway/scripts/autochangeWallpaper.sh
@@ -6,8 +6,9 @@ echo $WALLPAPER
 swaymsg "output * bg $HOME/.config/sway/wallpapers/'$WALLPAPER'  stretch"
 
 #Get some pics in some condition
+RANDOMNUM=$(($RANDOM%5+1))
 LINECOUNTS=`ls $HOME/.config/sway/wallpapers | wc -l`
-if [ $LINECOUNTS -le 10 ]
+if [ $RANDOMNUM -eq 2 ]
 then
   for loop in 1 2 3 4 5
   do
@@ -21,7 +22,6 @@ fi
 #Delete some politics
 if [ $LINECOUNTS -ge 10 ]
 then
-  echo `ls $HOME/.config/sway/wallpapers | sort -R | head -n 1`
   for loop in 1 2 3 4 5
   do
     rm $HOME/.config/sway/wallpapers/`ls $HOME/.config/sway/wallpapers | sort -R | head -n 1`
