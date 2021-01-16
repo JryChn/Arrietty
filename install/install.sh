ea116a090bd29976f1bd0fc9b031da9bbda28908
JeremyChen <22426265+JryChn@users.noreply.github.com>
2021-01-16 13:43:36 +0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -9,12 +9,14 @@
 # ====================================================
 
 #need install ##====================================== ##operating system
+
+efibootmgr -c --disk /dev/sdx --part x --label "xxxxx" -u "rw initrd= root=/dev/sdx" -l "\efi\boot\bootx64.efi"
 emerge -avuDN networkmanager-qt alsa-utils alsa-lib pulseaudio layman wayland
 vim laptop-mode-tools layman sddm fontawesome zsh fish fcitx ranger
 fcitx-configtool fcitx-rime dev-vcs/git curl wget tmux rofi trojan qutebrowser
 virt-manager qemu edk2-ovmf libvirt firewalld vlc libmpdclient wqy-microhei
 grim ffmpeg gui-apps/mako pavucontrol google-chrome light w3m sudo nodejs
 proxychains x11-terms/termite media-fonts/nerd-fonts gui-apps/gammastep waybar
-clang htop neofetch yarn gdb
+clang htop neofetch yarn gdb sway
 layman -a thegreatmcpain sorrow gig gentoo-zh curl -sLf
 https://spacevim.org/install.sh | bash 
